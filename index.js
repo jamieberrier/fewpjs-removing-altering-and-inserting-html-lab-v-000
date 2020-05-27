cea20b96ccdbdec96bde12a78b28488641fbf27e
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-27 20:17:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Write your code here!
 main.remove();
+
 let newHeader =  document.createElement('h1')
 newHeader.id = 'victory';
+
+let name = 'Jamie'
+header = document.getElementById('h1#victory')
