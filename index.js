597a1ed666daaa95e77dec4795248c192b3e9c1b
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-27 20:23:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ main.remove();
 let newHeader =  document.createElement('h1');
 newHeader.id = 'victory';
 
-header = document.getElementById("h1#victory");
+header = document.querySelector("h1#victory");
 header.innerHTML = "Jamie is the champion";
