3f03b821a0f0f839fd4977e7c789272c464eed7c
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-27 20:18:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your code here!
 main.remove();
 
-let newHeader =  document.createElement('h1')
+let newHeader =  document.createElement('h1');
 newHeader.id = 'victory';
 
-let name = 'Jamie'
-header = document.getElementById('h1#victory')
-header.innerHTML = ``
+let name = 'Jamie';
+header = document.getElementById('h1#victory');
+header.innerHTML = `${name} is the champion`;
